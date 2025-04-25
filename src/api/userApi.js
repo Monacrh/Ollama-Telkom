f46b645c7e494822b2d3a61c81b0b37cf2cfc7a7
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2025-04-25 21:23:42 +0700

    feat: Implementasi endpoint user

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -9,3 +9,43 @@ export const fetchUser = async () => {
     throw error;
   }
 };
+
+export const fetchListStudents = async () => {
+  try {
+    const response = await apiClient.get('/user/students');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching students list:', error);
+    throw error;
+  }
+}
+
+export const fetchListLecturers = async () => {
+  try {
+    const response = await apiClient.get('/user/lecturer');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching lecturers list:', error);
+    throw error;
+  }
+}
+
+export const fetchListModels = async () => {
+  try {
+    const response = await apiClient.get('/user/models');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching models list:', error);
+    throw error;
+  }
+}
+
+export const updateUser = async (userData) => {
+  try {
+    const response = await apiClient.put('/user/profile', userData);
+    return response.data;
+  } catch (error) {
+    console.error('Error updating user data:', error);
+    throw error;
+  }
+}
--- src/stores/slices/userSlice.js
+++ src/stores/slices/userSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { fetchUser } from '../../api/userApi';
+import { fetchUser, fetchListStudents, fetchListLecturers, fetchListModels, updateUser } from '../../api/userApi';
 
 const initialUser = {
   userID: '',
@@ -11,10 +11,17 @@ const initialUser = {
   deletedAt: null,
 };
 
+const initialStudents = [];
+const initialLecturers = [];
+const initialModels = [];
+
 export const userSlice = createSlice({
   name: 'user',
   initialState: {
     user: initialUser,
+    students: initialStudents,
+    lecturers: initialLecturers,
+    models: initialModels,
     status: 'idle',
     error: null,
   },
@@ -46,6 +53,48 @@ export const userSlice = createSlice({
         state.status = 'failed';
         state.user = initialUser;
         state.error = 'Failed to fetch user data';
+      })
+
+      .addCase(getListStudentsAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getListStudentsAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.students = action.payload;
+        state.error = null;
+      })
+      .addCase(getListStudentsAsync.rejected, (state) => {
+        state.status = 'failed';
+        state.students = initialStudents;
+        state.error = 'Failed to fetch user data';
+      })
+
+      .addCase(getListLecturersAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getListLecturersAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.lecturers = action.payload;
+        state.error = null;
+      })
+      .addCase(getListLecturersAsync.rejected, (state) => {
+        state.status = 'failed';
+        state.lecturers = initialLecturers;
+        state.error = 'Failed to fetch user data';
+      })
+
+      .addCase(getListModelsAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getListStudentsAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.models = action.payload;
+        state.error = null;
+      })
+      .addCase(getListStudentsAsync.rejected, (state) => {
+        state.status = 'failed';
+        state.models = initialModels;
+        state.error = 'Failed to fetch user data';
       });
   },
 });
@@ -60,5 +109,25 @@ export const selectError = (state) => state.user.error;
 
 export const getUserAsync = createAsyncThunk('user/getUser', async () => {
   const response = await fetchUser();
+  return response.data;
+});
+
+export const getListStudentsAsync = createAsyncThunk('user/getListStudents', async () => {
+  const response = await fetchListStudents();
+  return response.data;
+});
+
+export const getListLecturersAsync = createAsyncThunk('user/getListLecturers', async () => {
+  const response = await fetchListLecturers();
+  return response.data;
+});
+
+export const getListModelsAsync = createAsyncThunk('user/getListModels', async () => {
+  const response = await fetchListModels();
+  return response.data;
+});
+
+export const updateUserAsync = createAsyncThunk('user/updateUser', async (userData) => {
+  const response = await updateUser(userData);
   return response.data;
 });
